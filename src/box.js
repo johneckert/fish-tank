40a10d4cf8f1682b398d05cf21beda8225aab3d0
John Eckert <johnteckert@gmail.com>
2024-02-05 11:07:39 -0500

    remove threeJS code

--- src/box.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three';
-
-// Create box:
-const boxGeometry = new THREE.BoxGeometry(2, 2, 2); // Define geometry
-const boxMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF}); // Define material
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial); // Build box
-
-export default boxMesh;
--- src/index.js
+++ src/index.js
@@ -1,57 +1,14 @@
-import "./styles.css"
-import boxMesh from "./box";
-import * as THREE from 'three';
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+const frameWidth = 100;
+const frameHeight = 100;
+const xPos = 100;
+const yPos = 100;
+const scale = 1;
+const fps = 60;
+const secondsToUpdate = 1 * fps;
+let count = 0;
 
-// Scene
-const scene = new THREE.Scene();
-// Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.6, 1200);
-camera.position.z = 5; // Set camera position
-// Renderer
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setClearColor("#233143"); // Set background colour
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Make Canvas Responsive
-window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth, window.innerHeight); // Update size
-    camera.aspect = window.innerWidth / window.innerHeight; // Update aspect ratio
-    camera.updateProjectionMatrix(); // Apply changes
-})
-
-scene.add(boxMesh); // Add box to canvas
-
-// Lights
-const lights = [];
-const lightHelpers = [];
-const lightValues = [
-    {colour: 0xFFFFFF, intensity: 20, dist: 0, x: 1, y: 0, z: 8},
-];
-for (let i=0; i<1; i++) {
-    lights[i] = new THREE.PointLight(
-        lightValues[i]['colour'], 
-        lightValues[i]['intensity'], 
-        lightValues[i]['dist']);
-    lights[i].position.set(
-        lightValues[i]['x'], 
-        lightValues[i]['y'], 
-        lightValues[i]['z']);
-    scene.add(lights[i]);
-
-    // Add light helpers for each light
-    lightHelpers[i] = new THREE.PointLightHelper(lights[i]);
-    scene.add(lightHelpers[i]);
-}
-
-// Axes Helper
-const axesHelper = new THREE.AxesHelper(5);
-scene.add( axesHelper ); // X axis = red, Y axis = green, Z axis = blue
-
-
-const rendering = function() {
-    // Rerender every time the page refreshes (pause when on another tab)
-    requestAnimationFrame(rendering);
-    renderer.render(scene, camera);
-}
-rendering();
+canvas.style.marginTop = '0px';
