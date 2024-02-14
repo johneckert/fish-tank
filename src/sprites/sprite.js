5d83f9af75c544319e6b188e6fbc3531a8660679
John Eckert <johnteckert@gmail.com>
2024-02-14 11:11:41 -0500

    refactor

--- src/sprites/sprite.js
+++ src/sprites/sprite.js
@@ -1,3 +1,5 @@
+import TWEEN from 'tween.js';
+
 class Sprite {
   constructor(context, posX, posY, img, frameWidth, frameHeight, speed) {
     this.context = context;
@@ -34,6 +36,26 @@ class Sprite {
 
     if (this.frameIndex > this.endFrame) {
       this.frameIndex = 0;
+    }
+  }
+
+  move() {
+    if (this.tick === this.speed) {
+      if (this.x > this.context.canvas.width) {
+        this.x = -this.frameWidth;
+      }
+
+      let pos = { x: this.x, y: this.y };
+      
+      this.tween = new TWEEN.Tween(pos).to({ x: pos.x + this.speed }, 1000 / 60 * this.speed);
+
+      this.tween.onUpdate(() => {
+        this.x = pos.x;
+      });
+      this.tween.start();
+      this.tick = 0;
+    } else {
+      this.tick++;
     }
   }
 }
